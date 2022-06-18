6e8562ae0c50c3cf5c245dfcb231f33b9f7c84ee
Mertyon <81264329+Mertyon@users.noreply.github.com>
2022-06-18 20:53:09 +0200

    Simple Game
    Done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,74 @@
 package com.company;
 
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("hello");
+        Code.GuessNumberGame();
+    }
+
+
+    public class Code {
+
+        public static void
+        GuessNumberGame() {
+
+            // Scanner Class
+            Scanner sc = new Scanner(System.in);
+
+            // Generate Random Number
+            int number = 1 + (int) (100
+                    * Math.random());
+
+            // Given X Trials
+            int X = 5;
+
+            int i, guess;
+
+            System.out.println(
+                    "A number is chosen"
+                            + " between 1 to 100."
+                            + "Guess the number"
+                            + " within 5 trials.");
+
+            // Iterate Over
+            for (i = 0; i < X; i++) {
+
+                System.out.println(
+                        "Guess the number:");
+
+                // User Input
+                guess = sc.nextInt();
+
+                // Guess Was Right
+                if (number == guess) {
+                    System.out.println(
+                            "Congratulations!"
+                                    + " You guessed right!");
+                    break;
+                } else if (number > guess
+                        && i != X - 1) {
+                    System.out.println(
+                            "The number is "
+                                    + "greater than " + guess);
+                } else if (number < guess
+                        && i != X - 1) {
+                    System.out.println(
+                            "The number is"
+                                    + " less than " + guess);
+                }
+            }
+
+            if (i == X) {
+                System.out.println(
+                        "You have exhausted"
+                                + " X trials.");
+
+                System.out.println(
+                        "The number was " + number);
+            }
+        }
     }
 }
